d002c34c2f3d6aca05ea863c8e5bcc4ad2e89b58
dava12321 <55383271+dava12321@users.noreply.github.com>
2025-05-19 21:41:24 +0700

    UI LOGIN,BY DHAVA

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Login } from './pages/Login'
+
+// import { BrowserRouter, Route, Routes } from 'react-router-dom'
+// import { Todo } from './pages/Todo'
+
+function App() {
+
+  return (
+   <Login/>
+  )
+}
+
+export default App
